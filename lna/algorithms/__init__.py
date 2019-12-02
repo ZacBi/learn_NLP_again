0a03adb1853287a3692633dab3a935a6c0c000f6
zacbi <2535181990@qq.com>
2019-12-03 01:41:09 +0800

    again

--- lna/algorithms/__init__.py
+++ lna/algorithms/__init__.py
@@ -1,6 +1,6 @@
 """algorithms"""
 from lna.algorithms.bpe import byte_pair_encoding
 from lna.algorithms.min_edit_distance import (
-    min_edit_distance,
-    min_edit_distance_pro,
+        min_edit_distance,
+        min_edit_distance_pro,
 )
