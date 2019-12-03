986af96c22aa94014634cae285c70380b553e7a2
zacbi <2535181990@qq.com>
2019-12-03 10:11:33 +0800

    yapf & pylint

--- lna/algorithms/__init__.py
+++ lna/algorithms/__init__.py
@@ -1,6 +1,3 @@
 """algorithms"""
 from lna.algorithms.bpe import byte_pair_encoding
-from lna.algorithms.min_edit_distance import (
-        min_edit_distance,
-        min_edit_distance_pro,
-)
+from lna.algorithms.min_edit_distance import min_edit_distance, min_edit_distance_pro
